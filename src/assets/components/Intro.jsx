a2fd1c98606b57297a6eea6c71327d49517fe7ce
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-06-01 14:51:08 +0100

    chore: Update Intro component styles and content

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -4,12 +4,18 @@ import { Fade } from "react-awesome-reveal";
 
 const Intro = () => {
     return (
-        <Fade>
-        <div className='section text-center d-flex'>
-            <h1>Hi, I'm <span className='highlight'>Gian Clauidio S.</span></h1>
-            <p>Software Engineer</p>
-        </div>
-        </Fade>
+        <div className='section d-flex flex-column align-items-center justify-content-center '>
+            <Fade>
+                <h1 className='row bg-danger'>Hi, I'm <span className='highlight'>Gian Clauidio S.</span></h1>
+                <div className="row bg-success">
+                    <p>I am currently base in London just graduated at Le Wagon Bootcamp,
+                        in webdevelopent intense course.
+                        I have a profound of developing tools and workflow,
+                        and how users can get the best of if.</p>
+                </div>      
+            </Fade>
+
+            </div >  
     )
 }
 
