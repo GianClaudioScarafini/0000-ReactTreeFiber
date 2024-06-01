68bb1ce96a5d1dc22b524542ac2b57138acf5dd3
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-06-01 11:55:47 +0100

    chore: Update styles for Intro component and add Bootstrap CSS

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./Intro.css"
-
+import { Fade } from "react-awesome-reveal";
 
 const Intro = () => {
     return (
-        <div className='section'>Intro</div>
+        <Fade>
+        <div className='section text-center d-flex'>
+            <h1>Hi, I'm <span className='highlight'>Gian Clauidio S.</span></h1>
+            <p>Software Engineer</p>
+        </div>
+        </Fade>
     )
 }
 
