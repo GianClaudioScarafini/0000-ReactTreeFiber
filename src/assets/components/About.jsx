bdd492762760f7b54ca501bf9d09fe192de9d99e
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-06-01 17:59:34 +0100

    chore: Add jQuery, Popper.js, and Bootstrap JS dependencies
    This commit adds the jQuery, Popper.js, and Bootstrap JS dependencies to the project. The dependencies are included in the index.html file using script tags. This update is necessary to enable the use of Bootstrap components and functionality in the project.

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
     return (
         <div className='section d-flex align-items-center justify-content-center row about-color'>
             <div className='row about'>
-                <div className='col-5 text-end  '>
+                <div className='col-lg-5 col-12 text-end'>
                     <h3>About</h3>
                     <h5>Give a brief summary of information about me.</h5>
                 </div>
-                <div className='row col-5 text-end text-'>
+                <div className='col-lg-5 col-12 text-end'>
                     <h6 className='text-start'>
                         I am currently working in the construction industry as a BIM Coordinator, managing various types of projects. I recently graduated from Le Wagon, where I developed a strong interest in web applications and tool creation.
                         My interest in coding began when I realized how it could drastically speed up the completion of small tasks. I started by using Python within visual programming tools and then moved on to creating small web scraping projects. Once I began, I discovered my passion for coding and decided to invest my time and resources into developing this skill further.
