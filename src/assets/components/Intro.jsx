2bd0f5ea757789e705852cb3a0c107581ef3af91
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-06-02 11:23:18 +0100

    chore: Update Intro component styles and content

--- src/assets/components/Intro.jsx
+++ src/assets/components/Intro.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import "./Intro.css"
 import { Fade } from "react-awesome-reveal";
-
+import video from "../../../video/video_1.mp4"
 const Intro = () => {
     return (
-        <div className='section'>
-            <div className='canter'>
-                <Fade cascade damping={0.09} >
-                    <h1 className='intro'>Hello, I'm <span className='name'>Gian Claudio S</span></h1>
-                    <h2 className='intro'>I'm a Full Stack Developer</h2>
-                    <div>
-                        <p className='p-5'>
-                            I am currently base in London just graduated at Le Wagon Bootcamp,
-                            in webdevelopent intense course.
-                            I have a profound of developing tools and workflow,
-                            and how users can get the best of if.
-                        </p>
-                    </div>
-                </Fade>
+        <>
+            <div className="video">
+                <video autoPlay muted loop id="bg-video">
+                    <source src={video} type="video/mp4" />
+                    Your browser does not support HTML5 video.
+                </video>
+
+                <div className="intro-content">
+                    <Fade cascade damping={0.09} >
+                        <div className='intro-center'>
+                            <h1 className='intro-title'>Hello, I'm <span className='name'>Gian Claudio S</span></h1>
+                            <h2 className='intro-job'>I'm a Full Stack Developer</h2>
+                            <p className='intro-description'>
+                            Currently based in London, I've recently completed the intensive web development course at Le Wagon Bootcamp. My expertise lies in crafting efficient development tools and workflows, ensuring users can maximize their experience
+                            </p>
+                        </div>
+                    </Fade>
+
+
+                </div>
             </div>
-        </div >
+
+        </>
     )
 }
 
